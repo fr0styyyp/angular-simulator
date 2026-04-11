e697357e2aab069ab3d5bbf353a4688e4f8e7f1a
fr0styyyp <impost.side@gmail.com>
2026-04-11 16:33:53 +0500

    fixed issues 2

--- src/users-page/users-page.component.ts
+++ src/users-page/users-page.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../app/user.service';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, tap } from 'rxjs';
 import { IUser } from '../app/interfaces/IUser';
 import { UserCardComponent } from "../user-card/user-card.component";
 import { CreateUserComponent } from '../create-user/create-user.component';
@@ -17,6 +17,23 @@ export class UsersPageComponent implements OnInit {
   
   userService: UserService = inject(UserService);
   users$: Observable<IUser[]> = this.userService.users$;
+  private searchTermSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  searchTerm$: Observable<string> = this.searchTermSubject.asObservable();
+  
+  filteredUsers$: Observable<IUser[]> = combineLatest([
+  this.users$,
+  this.searchTerm$
+  ]).pipe(
+    map(([users, term]: [IUser[], string]) => {
+      const filterValue: string = term.toLowerCase().trim();
+      if (!filterValue) {
+        return users;
+      }
+      return users.filter((user: IUser) => 
+        user.name.toLowerCase().includes(filterValue)
+      );
+    })
+  );
   
   ngOnInit() {
     this.userService.loadUsers()
@@ -42,6 +59,11 @@ export class UsersPageComponent implements OnInit {
           this.userService.setUsers(data);
         })
       ).subscribe();
+  }
+  
+  onSearch(event: Event): void {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    this.searchTermSubject.next(input.value);
   }
   
 }
