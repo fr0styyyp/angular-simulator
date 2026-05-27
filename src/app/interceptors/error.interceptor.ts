51bc7c8b765cd9bfa6586e7b102789b5443d9597
fr0styyyp <impost.side@gmail.com>
2026-05-27 20:50:39 +0500

    fixed issues 1

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,9 +1,9 @@
-import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { MessageService } from '../message.service';
 import { inject } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
-export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+export const errorInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const messageService: MessageService = inject(MessageService);
   
   return next(req).pipe(
@@ -11,10 +11,11 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       const errorStatus: number = error.status;
       
       if (errorStatus === 0) {
-        messageService.showError('Отсутствует подключение к интернету')
+        messageService.showError('Отсутствует подключение к интернету');
       }
+      
       if (errorStatus >= 500) {
-        messageService.showError('Внутренняя ошибка сервера. Попробуйте позже')
+        messageService.showError('Внутренняя ошибка сервера. Попробуйте позже');
       }
       
       return throwError(() => error);
--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -1,7 +1,7 @@
-import { HttpErrorResponse, HttpEvent, HttpEventType, HttpInterceptorFn } from '@angular/common/http';
-import { finalize, tap } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpEventType, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { finalize, Observable, tap } from 'rxjs';
 
-export const loggingInterceptor: HttpInterceptorFn = (req, next) => {
+export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const start: number = performance.now();
   let status: number;
   
@@ -17,12 +17,13 @@ export const loggingInterceptor: HttpInterceptorFn = (req, next) => {
       }
     }),
     finalize(() => {
-      console.log(`HTTP метод запроса: ${req.method}`);
-      console.log(`URL запроса: ${req.url}`);
-      console.log(`Статус ответа: ${status}`);
       const end: number = performance.now();
       const duration: number = end - start;
-      console.log(`Время выполнения запроса: ${duration}`);
+      
+      console.log(`HTTP метод запроса: ${ req.method }`);
+      console.log(`URL запроса: ${ req.url }`);
+      console.log(`Статус ответа: ${ status }`);
+      console.log(`Время выполнения запроса: ${ duration.toFixed(2) } мс`);
     })
   );
 };
